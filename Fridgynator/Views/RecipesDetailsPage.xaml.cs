c980a4b20877c98e022a80befd0454233e4bc92d
TiinaTmk <119687726+TiinaTmk@users.noreply.github.com>
2023-12-15 13:25:58 +0200

    Tiina retseptid15.12

---
+++ Fridgynator/Views/RecipesDetailsPage.xaml.cs
@@ -0,0 +1,16 @@
+using Fridgynator.Models;
+
+namespace Fridgynator.Views
+{
+	public partial class RecipesDetailsPage : ContentPage
+	{
+		public RecipesDetailsPage(Recipes recipes)
+		{
+			InitializeComponent();
+		
+		}
+
+		
+	}
+
+}
