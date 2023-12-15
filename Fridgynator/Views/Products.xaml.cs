bda2697e372642eb44645daa0f7dad454959e14a
Diana Sedman <dianasedman@gmail.com>
2023-12-15 12:33:48 +0200

    products page started

--- Fridgynator/Views/Products.xaml.cs
+++ Fridgynator/Views/Products.xaml.cs
@@ -1,9 +1,26 @@
+using Fridgynator.Models;
+using System.Collections.ObjectModel;
+
 namespace Fridgynator.Views;
 
 public partial class Products : ContentPage
 {
-	public Products()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<ProductsModel> ProductsList { get; set; }
+
+    public Products()
+    {
+        InitializeComponent();
+
+        ProductsList = new ObservableCollection<ProductsModel>
+            {
+                new ProductsModel { ImageSource = "bananas.png", Title = "Bananas" },
+                new ProductsModel { ImageSource = "cheese.png", Title = "Cheese" },
+                new ProductsModel { ImageSource = "eggs.png", Title = "Eggs" },
+                new ProductsModel { ImageSource = "meat.png", Title = "Meat" },
+                new ProductsModel { ImageSource = "mustard.png", Title = "Mustard" },
+            };
+
+        BindingContext = this;
+
+    }
 }
